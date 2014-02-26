7dcb45557bf3c9d478bae3ae69a4d6258ffa8ce4
Nazario A. Ayala <nazario@niztech.com>
2014-02-26 10:07:56 -0800

    Starting to experiment with external displays.

---
+++ DerbyScoreBoard/NIZExternalDisplay.h
@@ -0,0 +1,13 @@
+//
+//  NIZExternalDisplay.h
+//  DerbyScoreBoard
+//
+//  Created by Nazario Ayala on 2/19/14.
+//  Copyright (c) 2014 Nazario A. Ayala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NIZExternalDisplay : UIViewController
+
+@end
---
+++ DerbyScoreBoard/NIZExternalDisplay.m
@@ -0,0 +1,38 @@
+//
+//  NIZExternalDisplay.m
+//  DerbyScoreBoard
+//
+//  Created by Nazario Ayala on 2/19/14.
+//  Copyright (c) 2014 Nazario A. Ayala. All rights reserved.
+//
+
+#import "NIZExternalDisplay.h"
+
+@interface NIZExternalDisplay ()
+
+@end
+
+@implementation NIZExternalDisplay
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
