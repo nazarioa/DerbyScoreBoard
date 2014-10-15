f2d010e7378b36ed9e4d06c5454a05789049e2d3
Nazario A. Ayala <nazario@niztech.com>
2014-10-14 23:22:30 -0700

    Changed the name of the project and added splash screen

---
+++ DerbyScoreBoard/NIZLaunchScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  NIZLaunchScreenViewController.h
+//  DerbyScoreBoard
+//
+//  Created by Nazario A. Ayala on 10/14/14.
+//  Copyright (c) 2014 Nazario A. Ayala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NIZLaunchScreenViewController : UIViewController
+
+@end
